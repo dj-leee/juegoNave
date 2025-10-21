135ce80dfab5ae768a2499fe551cfa0521482e28
dj-leee <djocundadiawara@gmail.com>
2025-10-22 01:34:28 +0200

    Acción de apagado y encendido hecho

--- js/main.js
+++ js/main.js
@@ -5,6 +5,16 @@ const btApagar = document.getElementById("teclaQ")
 document.addEventListener("keydown", (event) => {
     if (event.key === "e" || event.key === "E") {
         nave.classList.add("encender")
+    } else if (event.key === "q" || event.key === "Q") {
+        nave.classList.remove("encender")
     }
+    
+}) // <- Faltaba cerrar aquí
 
-    }
+btEncender.addEventListener("click", () => {
+    nave.classList.add("encender")
+})
+
+btApagar.addEventListener("click", () => {
+    nave.classList.remove("encender")
+})

8723bc6d2a0aefaf15d8822bed307f8bddc46c84
dj-lee <djocundadiawara@gmail.com>
2025-10-23 19:52:32 +0200

    haciendo segundo metodo optimizado

--- js/main.js
+++ js/main.js
@@ -1,43 +1,142 @@
+class NaveEspacial {
+  constructor(nombre, tamano, direccion, motor, velocidad) {
+    this.nombre = nombre;
+    this.tamano = tamano;
+    this.direccion = direccion;
+    this.motor = motor;
+    this.velocidad = velocidad;
+  }
+
+  // Metodo para encender el motor de la nave
+  encenderMotor() {
+    this.motor = true;
+    motor.classList.add("encender");
+  }
+
+  // Metodo para apagar el motor de la nave
+  apagarMotor() {
+    this.motor = false;
+    motor.classList.remove("encender");
+  }
+
+  // Metodo para mover la nave en una direccion especifica
+  mover(direccion) {
+    if (this.motor) {
+      this.direccion = direccion;
+
+      avanzar -= this.velocidad;
+
+      estructura.style.transform = `translateY(${avanzar}px)`;
+    }
+  }
+  // Metodo para girar la nave
+    girar(direcciongiro){
+        if(direcciongiro === "sur"){
+            estructura.style.transform = `rotate(90deg) translateY(${avanzar}px) origin: center bottom;`
+        }
+    }
+
+  // Metodo para cambiar la velocidad de la nave
+  cambiarVelocidad(nuevavelocidad) {
+    this.velocidad = nuevavelocidad;
+  }
+}
+
+// Crear una instancia de la clase NaveEspacial
+const naveExplorer = new NaveEspacial(
+  "Explorer",
+  "Mediana",
+  "Norte",
+  false,
+  10
+);
+const naveVoyager = new NaveEspacial("Voyager", "Grande", "Norte", false, 15);
+
 // VARIABLES SIMPLES Y CONSTANTES
-const nave = document.getElementById("nave")
-const estructura = document.getElementById("estructura")
-const motor = document.getElementById("motor")
-const motorEncendido = motor.classList.contains("encender")
-
-let posY = 0
-
-
-
+const nave = document.getElementById("nave");
+const estructura = document.getElementById("estructura");
+const motor = document.getElementById("motor");
+const motorEncendido = motor.classList.contains("encender");
+let avanzar = 0;
+// Segun la pulsacion de teclas, vamos a llamar a los metodos de la clase NaveEspacial
+// Funcion de escuchar si pulsan una tecla
 document.addEventListener("keydown", (event) => {
-switch (event.key.toLowerCase()) {
+  switch (event.key.toLowerCase()) {
     case "e":
-        motor.classList.add("encender")
-        break;
-
+      // Encender el motor
+      naveExplorer.encenderMotor();
+      break;
     case "q":
-        motor.classList.remove("encender")
-        break;
-        m
+      // Apagar el motor
+      naveExplorer.apagarMotor();
+      break;
     case "arrowup":
-        case "w":
-        if (motor.classList.contains("encender")) {
-        posY -= 10
-        estructura.style.transform = `translateY(${posY}px)`
-        }
-        break;
-
+    case "w":
+      naveExplorer.mover("Norte");
+      break;
     case "arrowdown":
-        case "s":
-        if (motor.classList.contains("encender")) {
-        posY += 10
-        estructura.style.transform = `translateY(${posY}px)`
-        }
-        break;
+    case "s":
+      naveExplorer.girar("sur");
+      break;
+    case "arrowrigth":
+    case "d":
+      naveExplorer.girar("Este");
+      break;
+    case "arrowleft":
+    case "a":
+      naveExplorer.girar("Oeste");
+      break;
+
 
     default:
-        console.log("Tecla no reconocida");
-        break;
-}
-}) 
+      break;
+  }
+});
 
+// Pulsar teclas para controlar la nave
+// document.addEventListener("keydown", (event) => {
+//     switch (event.key.toLowerCase()) {
+//         case "e":
+//             // Encender el motor
+//             motor.classList.add("encender");
+//             break;
+//         case "q":
+//             // Apagar el motor
+//             motor.classList.remove("encender");
+//             break;
+//         case "arrowup":
+//         case "w":
+//             if (motor.classList.contains("encender")) {
+//                 avanzar -= 10;
+//                 estructura.style.transform = `translateY(${avanzar}px)`;
+//                 // mover la nave con la flecha arriba o la tecla w solo si el motor está encendido
+//             }
+//             break;
+//         case "arrowright":
+//         case "d":
+//             // girar la nave a la derecha con la flecha derecha o la tecla d
+//             if (motor.classList.contains("encender")) {
+//                 girarDer += 90;
+//                 estructura.style.transform = `translateY(${avanzar}px) rotate(${girarDer}deg)`;
+//             }
+//             break;
+//         case "arrowleft":
+//         case "a":
+//             // girar la nave a la izquierda con la flecha izquierda o la tecla a
+//             if (motor.classList.contains("encender")) {
+//                 girarIzq -= 90;
+//                 estructura.style.transform = `translateY(${avanzar}px) rotate(${girarIzq}deg)`;
+//             }
+//             break;
+//         default:
+//             break;
+//     }
+// });
 
+// Sino se activan los motores va cayendo la nave
+// setInterval(() => {
+//     if (!motor.classList.contains("encender")) {
+//         posicionY += 5;
+//         nave.style.transform = `translateY(${posicionY}px)`;
+//     }
+// }, 1000);
